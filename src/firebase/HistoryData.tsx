39ee23f47beb502c6772d1b631c24282c607bf72
amirziou <amirziouziou12@gmail.com>
2023-08-24 12:59:34 +0100

    regroupement sem jour heure

--- src/firebase/HistoryData.tsx
+++ src/firebase/HistoryData.tsx
@@ -4,12 +4,16 @@ import { Chaine } from "../firebase/ExtractingData";
 import axiosClient from "../firebase/axios-client";
 import { db } from "../../config";
 import { onValue, ref } from "firebase/database";
+import { Divider, HStack, Heading, SimpleGrid, Text } from "@chakra-ui/react";
+
 let b = 0;
 interface GroupedData {
-  [dateKey: string]: {
-    [hour: string]: {
-      cb: number;
-      cm: number;
+  [weekNumber: string]: {
+    [dateKey: string]: {
+      [hour: string]: {
+        cb: number;
+        cm: number;
+      };
     };
   };
 }
@@ -18,6 +22,14 @@ const HistoryData = () => {
   const [ONEchaineArray, setONEchaineArray] = useState<Chaine>([]);
   const { id } = useParams();
   const [organizedData, setOrganizedData] = useState<GroupedData>({});
+
+  const getWeekNumber = (date: Date) => {
+    const oneJan = new Date(date.getFullYear(), 0, 1);
+    const timeDiff = date.getTime() - oneJan.getTime();
+    const dayOfYear = Math.ceil(timeDiff / (24 * 60 * 60 * 1000));
+    const weekNumber = Math.ceil(dayOfYear / 7);
+    return weekNumber;
+  };
 
   useEffect(() => {
     console.log("start extracting one chaine data");
@@ -56,42 +68,57 @@ const HistoryData = () => {
       const mois = item.timestamp.getMonth() + 1;
       const annee = item.timestamp.getFullYear();
       const dateKey = jour + "/" + mois + "/" + annee;
+      const weekNumber = getWeekNumber(item.timestamp);
 
-      if (!groupedData[dateKey]) {
-        groupedData[dateKey] = {};
+      if (!groupedData[weekNumber]) {
+        groupedData[weekNumber] = {};
       }
-      if (!groupedData[dateKey][heure]) {
-        groupedData[dateKey][heure] = { cb: 0, cm: 0 };
+      if (!groupedData[weekNumber][dateKey]) {
+        groupedData[weekNumber][dateKey] = {};
+      }
+      if (!groupedData[weekNumber][dateKey][heure]) {
+        groupedData[weekNumber][dateKey][heure] = { cb: 0, cm: 0 };
       }
 
       let a = Number(item.cb);
       let b = Number(item.cm);
 
-      groupedData[dateKey][heure].cb += a;
-      groupedData[dateKey][heure].cm += b;
+      if (!groupedData[weekNumber][dateKey]) {
+        groupedData[weekNumber][dateKey][heure] = { cb: 0, cm: 0 };
+      }
+      groupedData[weekNumber][dateKey][heure].cb += a;
+      groupedData[weekNumber][dateKey][heure].cm += b;
+      console.log(groupedData);
 
       setOrganizedData(groupedData);
     });
   }, [ONEchaineArray]);
 
   return (
-    <>
-      <div className="App">
-        <h4>Données Organisées en jour et en heure</h4>
-        {Object.keys(organizedData).map((dateKey) => (
-          <div key={dateKey}>
-            <h2>{dateKey}</h2>
-            {Object.keys(organizedData[dateKey]).map((heure) => (
-              <div key={heure}>
-                <p>Heure: {heure}:00</p>
-                <p>CB: {organizedData[dateKey][heure].cb}</p>
-                <p>CM: {organizedData[dateKey][heure].cm}</p>
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
-    </>
+    <div className="App">
+      <Text fontSize={30}>Données Organisées en jour et en heure</Text>
+      {Object.keys(organizedData).map((weekNumber) => (
+        <div key={weekNumber}>
+          <Text fontSize={25}>{`Week: ${weekNumber}`}</Text>
+          {Object.keys(organizedData[weekNumber]).map((dateKey) => (
+            <div key={dateKey}>
+              <Text fontSize={20}>{dateKey}</Text>
+              {Object.keys(organizedData[weekNumber][dateKey]).map((heure) => (
+                <div key={heure}>
+                  <Text fontSize={15}>Heure: {heure}:00</Text>
+                  <Text>
+                    CB: {organizedData[weekNumber][dateKey][heure].cb}
+                  </Text>
+                  <Text>
+                    CM: {organizedData[weekNumber][dateKey][heure].cm}
+                  </Text>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
   );
 };
 export default HistoryData;
